f0f6f0c191332052af8fed498081e3566b4b656c
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2023-12-09 19:04:17 +0400

    change dashboard

---
+++ app/(dashboard)/main/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1 className="text-secondary">Ak unda iyos kontenti</h1>
+    </div>
+  );
+};
+
+export default MainPage;
